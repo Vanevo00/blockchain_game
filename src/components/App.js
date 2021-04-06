065bcbd64635774e03a691bb2b4e3f1c2421d23c
Vanek <vanevo00@gmail.com>
2021-04-06 20:18:24 +0200

    feat: load web3 and load blockchain data functions

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,51 @@ class App extends Component {
     }
   }
 
+  async componentDidMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId()
+    const networkData = MemoryToken.networks[networkId]
+    if(networkData) {
+      const abi = MemoryToken.abi
+      const address = networkData.address
+      const token = new web3.eth.Contract(abi, address)
+      this.setState({ token })
+      const totalSupply = await token.methods.totalSupply().call()
+      this.setState({ totalSupply })
+      let balanceOf = await token.methods.balanceOf(accounts[0]).call()
+      for (let i = 0; i < balanceOf; i++) {
+        let id = await token.methods.tokenOfOwnerByIndex(accounts[0], i).call()
+        let tokenURI = await token.methods.tokenURI(id).call()
+        this.setState({
+          tokenURIs: [...this.state.tokenURIs, tokenURI]
+        })
+      }
+    } else {
+      alert('Smart contract not deployed to detected network.')
+    }
+  }
+
   render() {
     return (
       <div>
